0b1ff01c48495dba704f78d193a7c5fa1f116719
wonJJun2 <ywj2321@naver.com>
2026-09-15 21:42:36 +0900

    fix: remove unresolved conflict markers

--- src/game_data_platform/__init__.py
+++ src/game_data_platform/__init__.py
@@ -24,21 +24,12 @@ def main() -> None:
     games = [
         {"name": "THE FINALS", "app_id": 2073850},
         {"name": "Overwatch", "app_id": 2357570},
-<<<<<<< HEAD
-		{"name": "PUBG: BATTLEGROUNDS", "app_id": 578080},
-		{"name": "Counter-Strike 2", "app_id": 730},
-		{"name": "Apex 레전드", "app_id": 1172470},
-		{"name": "마블 라이벌즈", "app_id": 2767030},
-		{"name": "Dota 2", "app_id": 570},
-		{"name": "퍼스트 디센던트", "app_id": 2074920}
-=======
         {"name": "PUBG: BATTLEGROUNDS", "app_id": 578080},
         {"name": "Counter-Strike 2", "app_id": 730},
         {"name": "Apex 레전드", "app_id": 1172470},
         {"name": "마블 라이벌즈", "app_id": 2767030},
         {"name": "Dota 2", "app_id": 570},
         {"name": "퍼스트 디센던트", "app_id": 2074920},
->>>>>>> 4817797 (feat: add error handling and data persistence)
     ]
 
     for game in games:
